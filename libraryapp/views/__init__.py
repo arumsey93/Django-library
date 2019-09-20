50828678b3c3cc050781c4cebbe63ff3757159f0
Alex Rumsey <alexrumsey1993@gmail.com>
2019-09-20 10:25:00 -0500

    added editing

--- libraryapp/views/__init__.py
+++ libraryapp/views/__init__.py
@@ -2,8 +2,8 @@ from .books.list import book_list
 from .librarians.list import list_librarians
 from .home import home
 from .auth.logout import logout_user
-from .books.form import book_form
 from .libraries.list import library_list
 from .libraries.form import library_form
 from .books.details import book_details
 from .libraries.details import library_details
+from .books.form import book_form, book_edit_form
