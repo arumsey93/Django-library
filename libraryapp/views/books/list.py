c2986c1f9f07b499d01fe5bba6611832e643a909
Alex Rumsey <alexrumsey1993@gmail.com>
2019-09-20 09:51:52 -0500

    updated for completed practices

--- libraryapp/views/books/list.py
+++ libraryapp/views/books/list.py
@@ -1,9 +1,12 @@
 import sqlite3
+from django.urls import reverse
+from django.shortcuts import redirect
 from django.shortcuts import render
 from libraryapp.models import Book
 from ..connection import Connection
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def book_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
@@ -43,3 +46,23 @@ def book_list(request):
         }
 
         return render(request, template, context)
+
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            INSERT INTO libraryapp_book
+            (
+                title, author, isbn,
+                published_year, location_id, librarian_id
+            )
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            (form_data['title'], form_data['author'],
+                form_data['isbn'], form_data['published_year'],
+                request.user.librarian.id, form_data["location"]))
+
+        return redirect(reverse('libraryapp:books'))
